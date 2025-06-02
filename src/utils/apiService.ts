33553fc83a703d457e6c6723e185514f5cdc0701
tete-dunai <121575041+tete-dunai@users.noreply.github.com>
2025-06-02 18:07:56 +0530

    Update apiService.ts

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -15,7 +15,7 @@ interface CalculationResponse {
   output: FYData[];
 }
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://tetedunai.pythonanywhere.com/';
 
 export const calculateRNORStatus = async (data: CalculationRequest): Promise<FYData[]> => {
   try {
